88261954325078a61da24ca0686bd877e2fe2527
tcoolican <tcoolican@usf.edu>
2024-09-14 14:56:14 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -5,6 +5,15 @@ let Inventory = [{name: "Schutt F7", Price: 499.99, Quantity: 150, lowStockLevel
 {name: "VICIS Zero2", Price: 799.99, Quantity: 75, lowStockLevel: 5},
 {name: "Xenith Varsity Shadow", Price: 449.99, Quantity: 50, lowStockLevel: 5},
 {name: "Riddell Axiom", Price: 750.00, Quantity: 75, lowStockLevel: 5}
-]
+] //These are all the top of the line football helmets
 console.log(Inventory);
+
+// Task 2: Create a Function to Display Product Details
+function displayProductDetails(Product) {
+    const stockLevel = Product.Quantity <= Product.lowStockLevel ? "Low Stock" : "In Stock";
+    console.log(`Product Name: ${Product.name}`);
+    console.log(`Price: $${Product.Price}`);
+    console.log(`Quantity in Stock: ${Product.Quantity}`);
+    console.log(`Stock Status: ${stockLevel}`);
+}
 
