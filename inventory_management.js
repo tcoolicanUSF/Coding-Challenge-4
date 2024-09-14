97dfba257faa1528faaecc67bea20843de9ba238
tcoolican <tcoolican@usf.edu>
2024-09-14 13:44:32 -0400

    Initialize inventory with product objects

--- inventory_management.js
+++ inventory_management.js
@@ -1,1 +1,10 @@
+//Coding Challenge 4
+//Task 1: Create an Inventory Array of Product Objects
+let Inventory = [{name: "Schutt F7", Price: 499.99, Quantity: 150, lowStockLevel: 10},
+{name: "Riddell Speedflex", Price: 459.99, Quantity: 200, lowStockLevel: 20},
+{name: "VICIS Zero2", Price: 799.99, Quantity: 75, lowStockLevel: 5},
+{name: "Xenith Varsity Shadow", Price: 449.99, Quantity: 50, lowStockLevel: 5},
+{name: "Riddell Axiom", Price: 750.00, Quantity: 75, lowStockLevel: 5}
+]
+console.log(Inventory);
 
